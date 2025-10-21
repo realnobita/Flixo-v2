f0427ce10163cfcf5b0e6c6e6925f4e75c4b4ba6
Nobita <92166443+realnobita@users.noreply.github.com>
2025-10-21 18:49:44 +0530

    Create emoji.js

---
+++ src/config/emoji.js
@@ -0,0 +1,11 @@
+module.exports = {
+    author: "<:author:1341030386326831227>",
+    requester: "<a:requester:1341054606263980162>",
+    duration: "<:Duration:1341030738220552225>",
+    tick: "<:floovi_tick:1381965556277710860>",
+    cross: "<:1382029455601569904>",
+    playing: "<a:playing:1341030546989387796>",
+    warn: "<:floovi_warn:1382779289858211880>",
+    enable: "<:Enabled:1341056046306951200>",
+    disable: "<:Disabled:1341056047590277140>",
+};
