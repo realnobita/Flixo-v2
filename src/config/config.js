674e5b565746ebd2f0d21d285339220f7140eb45
Nobita <92166443+realnobita@users.noreply.github.com>
2025-10-21 18:48:21 +0530

    Create config.js

---
+++ src/config/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  token: "", // your bot token
+  prefix: "", // prefix
+  color: "", // color
+  Mongo: "",
+  ownerIDS: [""],
+  adminIDS: [""],
+  vote: true, // false if you dont have api key of DBL
+  invite: "",
+  ssLink: "",
+  dbl: "",
+  dblToken: "",
+  noprefixLog: "",
+  cmd_log: "",
+  error_log: "",
+  join_log: "",
+  leave_log: "",
+  spotiId: "",
+  spotiSecret: "",
+  nodes: [
+    {
+      name: "", // your lavalink name [ use v4 lavalink version for that ]
+       url: "", // url of your lavalink
+       auth: "", // password of your lavalink
+      secure: false, // ssl = true , non ssl = false
+       },
+  ],
+};
